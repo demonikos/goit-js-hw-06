788527b4f90c3792603b91741b9900967982135c
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-19 19:04:32 +0300

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+let addList = [];
+ingredients.forEach((element) => {
+  const ingredient = document.createElement("li");
+  ingredient.innerText = element;
+  ingredient.classList.add("item");
+  addList.push(ingredient);
+  }
+)
+list.append(...addList);
