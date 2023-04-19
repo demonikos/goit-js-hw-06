609d4b3eb40bcd11d78e6c62f93da191d2f02ef6
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-19 17:39:59 +0300

    first task

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const numberOfCatergories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${numberOfCatergories.length}`);
+numberOfCatergories.forEach(element => {
+    console.log('')
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.querySelectorAll('li').length}`);
+});
