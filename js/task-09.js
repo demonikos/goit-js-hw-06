676bff53b814b2ea0f1acc2658a753729f504597
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-20 14:47:12 +0300

    nineth task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const click = document.querySelector(".change-color");
+// const body = document.body;
+// const span = document.querySelector(".color");
+
+// click.addEventListener("click", (event) => {
+//   const color = getRandomHexColor();
+//   console.log(color);
+//   body.style.backgroundColor = color;
+//   span.textContent = color;
+// })
+
+
+const widget = document.querySelector(".widget");
+widget.lastElementChild.addEventListener("click", (event) => {
+      const color = getRandomHexColor();
+      // console.log(color);
+      document.body.style.backgroundColor = color;
+      widget.firstElementChild.firstElementChild.textContent = color;
+    }
+)
