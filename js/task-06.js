3c7fe3f585a30c7639b69eca2f612385955a4715
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-20 03:33:14 +0300

    sixth task

--- js/task-06.js
+++ js/task-06.js
@@ -1,2 +1,11 @@
-const inputLength = document.querySelector("#validation-input");
+const inputText = document.querySelector("#validation-input");
+inputText.addEventListener("blur", (event) => {
+    if (event.currentTarget.value.length !== Number(inputText.getAttribute("data-length"))){
+inputText.classList.remove("valid");
+inputText.classList.add("invalid");
+    } else {
+inputText.classList.remove("invalid");
+inputText.classList.add("valid");
+    }
 
+})
