d62db9d10cb6e4d157cebfaf66a2b447093b313f
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-20 04:00:40 +0300

    seventh task

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+const currValue = document.querySelector("#font-size-control");
+const currText = document.querySelector("#text");
+
+// currValue.addEventListener("input", (event) =>{
+//     // console.log(event.currentTarget.value);
+//     // console.log(typeof(Number(event.currentTarget.value)));
+//     currText.style.fontSize = event.currentTarget.value + "px";
+// })
+
+currValue.addEventListener("input", (event) => currText.style.fontSize = event.currentTarget.value + "px")
+
