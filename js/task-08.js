51bdd61ced1ce1eda6ad3a1f0911cfbc6588b9b5
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-21 20:45:17 +0300

    eighth task - doing obj and consoling obj

--- js/task-08.js
+++ js/task-08.js
@@ -2,24 +2,17 @@ const loginForm = document.querySelector(".login-form");
 
 loginForm.addEventListener("submit", (event) =>{
     event.preventDefault();  
-
-    // if (event.currentTarget.email.value === "" || event.currentTarget.password.value === ""){
-    //     alert("Warning, all datas must be input")
-    // }
-
     const {elements : {email, password}} = event.currentTarget;
     if (email.value === "" || password.value === ""){
         alert("Warning, all fields must be filled")
     } else {
-        console.log (`login - ${email.value}`);
-        console.log (`pass - ${password.value}`);
+        const user = {
+            email: email.value,
+            password: password.value
+        }
+        console.log (user);
         loginForm.reset();
+
+        // console.log(`user's mail - ${user.email}, user's password - ${user.password}`);
     }
-
-    // user.email = loginForm.elements.email.value;
-    // user.password = loginForm.elements.password.value;
-    // console.log(user);
-
-    // console.log (`login - ${loginForm.email.value}`);
-    // console.log (`pass - ${loginForm.password.value}`);
 })
