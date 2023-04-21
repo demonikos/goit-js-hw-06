3168bbe4cabe4b3583d7308e324ee9fb8a7658ea
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-21 20:15:58 +0300

    seventh task - adding curr value to text size

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,15 @@
 const currValue = document.querySelector("#font-size-control");
 const currText = document.querySelector("#text");
 
+
+
 // currValue.addEventListener("input", (event) =>{
 //     // console.log(event.currentTarget.value);
 //     // console.log(typeof(Number(event.currentTarget.value)));
 //     currText.style.fontSize = event.currentTarget.value + "px";
 // })
+
+currText.style.fontSize = currValue.value + "px";
 
 currValue.addEventListener("input", (event) => currText.style.fontSize = event.currentTarget.value + "px")
 
