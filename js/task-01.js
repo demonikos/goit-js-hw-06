db5749597b092ccfff5759ee2769e67214269926
demonikos <119507967+demonikos@users.noreply.github.com>
2023-04-21 19:42:36 +0300

    first task changing query on children

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,15 @@
+// const numberOfCatergories = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${numberOfCatergories.length}`);
+// numberOfCatergories.forEach(element => {
+//     console.log('')
+//     console.log(`Category: ${element.firstElementChild.textContent}`);
+//     console.log(`Elements: ${element.lastElementChild.querySelectorAll('li').length}`);
+// });
+
 const numberOfCatergories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${numberOfCatergories.length}`);
 numberOfCatergories.forEach(element => {
     console.log('')
     console.log(`Category: ${element.firstElementChild.textContent}`);
-    console.log(`Elements: ${element.lastElementChild.querySelectorAll('li').length}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
